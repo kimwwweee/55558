4e69221903a372b2bedc212d022bd6e24e070982
kimwwweee <cheonghak.t230206@ggh.goe.go.kr>
2025-06-17 12:04:42 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,294 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-5-1",
+      "name": "5층 1반",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.24777927241045994,
+        "pitch": -0.04253413655431437,
+        "fov": 0.6257244521336398
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7760639794661337,
+          "pitch": 0.32763852752611555,
+          "rotation": 0,
+          "target": "3-5-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5228618255570936,
+          "pitch": -0.14031343121537176,
+          "title": "진학상담실3",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-5-6",
+      "name": "5층 복도6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2217441978170633,
+          "pitch": 0.6116092173387457,
+          "rotation": 0,
+          "target": "4-52"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.6274624334667465,
+          "pitch": -0.006657077110443055,
+          "title": "공감교실",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.7601604762147769,
+          "pitch": -0.03393662089237992,
+          "title": "자기주도학습실3",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-5-",
+      "name": "5층 교무실",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2787600465990572,
+          "pitch": 0.4226322627569914,
+          "rotation": 0,
+          "target": "3-5-"
+        },
+        {
+          "yaw": 0.4272913568611596,
+          "pitch": 0.5205013927535411,
+          "rotation": 0,
+          "target": "4-52"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-5-",
+      "name": "5층 중앙",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.972479345665847,
+          "pitch": 0.19940369816512238,
+          "rotation": 0,
+          "target": "0-5-1"
+        },
+        {
+          "yaw": -1.2310767956593764,
+          "pitch": 0.11171768001941906,
+          "rotation": 0,
+          "target": "2-5-"
+        },
+        {
+          "yaw": -1.8680996650355244,
+          "pitch": 0.06066243317847153,
+          "rotation": 0,
+          "target": "5-adobe-express-file"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-52",
+      "name": "5층2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2593.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3138068226043558,
+          "pitch": 0.8203297502129221,
+          "rotation": 0,
+          "target": "1-5-6"
+        },
+        {
+          "yaw": 0.6628205299492524,
+          "pitch": 0.7122005876356905,
+          "rotation": 0,
+          "target": "2-5-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.736808365634376,
+          "pitch": 0.0925141306780013,
+          "title": "입시플랫폼",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-adobe-express-file",
+      "name": "Adobe-Express-file",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.22447294681727215,
+          "pitch": 0.26329129851770006,
+          "rotation": 0,
+          "target": "3-5-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
